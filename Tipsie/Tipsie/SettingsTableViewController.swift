0038092e1e793852c45e5d16f83e23716d94ef85
Marc A. Anderson <marc@marcadam.com>
2016-01-08 11:41:24 -0800

    Changed Settings page to a table view

--- Tipsie/Tipsie/SettingsTableViewController.swift
+++ Tipsie/Tipsie/SettingsTableViewController.swift
@@ -1,21 +1,28 @@
 //
-//  SettingsViewController.swift
+//  SettingsTableViewController.swift
 //  Tipsie
 //
-//  Created by Marc Anderson on 1/7/16.
+//  Created by Marc Anderson on 1/8/16.
 //  Copyright © 2016 Marc Anderson. All rights reserved.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsTableViewController: UITableViewController {
 
     @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+
         Tips.setTipControlText(tipPercentSegmentedControl)
+
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -35,4 +42,17 @@ class SettingsViewController: UIViewController {
     @IBAction func onDefaultTipValueChanged(sender: UISegmentedControl) {
         Tips.setDefaultTipIndex(tipPercentSegmentedControl.selectedSegmentIndex)
     }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+
 }
